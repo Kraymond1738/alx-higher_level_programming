2f42f3852c638a815dc0f4dc25855cee0850c856
Kasumba Raymond <kasumbaraymond0756519001@gmail.com>
2022-11-29 22:31:52 +0300

    New
    Up

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,16 +1,8 @@
-JK#ifndef LISTS_H
+#ifndef LISTS_H
 #define LISTS_H
 
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- *
- */
 
 typedef struct listint_s
 {
